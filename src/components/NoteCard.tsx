04aaedd656aa11f8ec72cebcfd0d41b9ae9c1875
Romario <romario.silva067@academico.ifs.edu.br>
2025-09-06 16:23:55 -0300

    ✨ Ajustes no notecard

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,28 +1,28 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import { Link } from "expo-router";
 import { Note } from "../types/note";
+import { useTheme } from "../styles/ThemeContext";
 
 type Props = {
   note: Note;
 };
 
 export default function NoteCard({ note }: Props) {
+  const { theme } = useTheme();
+
   return (
-    <Link
-      href={{ pathname: "../note/[id]", params: { id: String(note.id) } }}
-      asChild
-    >
+    <Link href={{ pathname: "./note/[id]", params: { id: String(note.id) } }} asChild>
       <TouchableOpacity
         style={{
           padding: 12,
           borderBottomWidth: 1,
-          borderColor: "#ccc",
+          borderColor: theme.card,
         }}
       >
-        <Text style={{ fontSize: 18 }}>
+        <Text style={{ fontSize: 18, color: theme.text }}>
           {note.mood} {note.title}
         </Text>
-        <Text style={{ color: "gray" }}>{note.date}</Text>
+        <Text style={{ color: theme.text }}>{note.date}</Text>
       </TouchableOpacity>
     </Link>
   );
